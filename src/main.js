83d2bfaa7a999dfa341b1d839b67d5341e3bd07a
devNattan <natanaelhenrique.s.c@gmail.com>
2023-01-16 15:26:32 -0300

    implementando a função

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 import { nanoid } from "nanoid";
 
+const button = document.querySelector('button');
+const generatedKey = document.querySelector('h2');
+
+button.addEventListener('click', () => {
+    const randomPassword = nanoid();
+    generatedKey.innerHTML = randomPassword
+});
