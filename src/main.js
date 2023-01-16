1dadf2f3ecb0811e980367f728ab15120cf80308
devNattan <natanaelhenrique.s.c@gmail.com>
2023-01-16 15:54:06 -0300

    aprimorando a aplicação

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { nanoid } from "nanoid";
 import "./style.css";
+import  copy  from "clipboard-copy"
+import Swal from 'sweetalert2'
 
 const button = document.querySelector('button');
 const generatedKey = document.querySelector('h2');
@@ -7,4 +9,16 @@ const generatedKey = document.querySelector('h2');
 button.addEventListener('click', () => {
     const randomPassword = nanoid();
     generatedKey.innerHTML = randomPassword
+});
+
+generatedKey.addEventListener('click', (event) => {
+    copy(event.target.innerHTML);
+    // alert('Senha copiada!');
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'You just copied your password',
+        showConfirmButton: false,
+        timer: 1800
+      })
 });
